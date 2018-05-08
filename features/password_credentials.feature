7149977864c40cb41c87d726136d83231885333f
Steve Franchak <stevefranchak@gmail.com>
2018-05-08 14:48:48 -0400

    Implement partial happy path unit test for PasswordCredentials using the BDD framework behave.

---
+++ features/password_credentials.feature
@@ -0,0 +1,14 @@
+Feature: Encapsulation for username/password credentials (irrespective of API)
+
+  Scenario Outline: The encapsulation should accept a valid username and password
+    Given we have username "<username>"
+      and we have password "<password>"
+    When we create an instance of the encapsulation
+    Then it should have the provided username and password
+      and be of the proper type and class
+      and not have a payload formatter
+
+    Examples: Valid Username/Password Pairs
+        | username      | password       |
+        | steve         | somepasswd     |
+        | aaaaaaaaaaaa1 | aaaaaaaaaaaaa2 |
---
+++ features/steps/password_credentials_steps.py
@@ -0,0 +1,30 @@
+# pylint: disable=E0611
+from behave import given, when, then, step
+from instatools.auth.password_credentials import PasswordCredentials
+from instatools.base.credentials import Credentials
+
+@given('we have username "{username}"')
+def have_username(context, username):
+    context.username = username
+
+@given('we have password "{password}"')
+def have_password(context, password):
+    context.password = password
+
+@when('we create an instance of the encapsulation')
+def create_credentials_instance(context):
+    context.credentials = PasswordCredentials(context.username, context.password)
+
+@then('it should have the provided username and password')
+def verify_username_and_password_match(context):
+    assert context.credentials.username == context.username
+    assert context.credentials.password == context.password
+
+@then('be of the proper type and class')
+def verify_password_credentials_type(context):
+    assert isinstance(context.credentials, PasswordCredentials)
+    assert isinstance(context.credentials, Credentials)
+
+@then('not have a payload formatter')
+def verify_payload_formatter_does_not_exist(context):
+    assert context.credentials.hasPayloadFormatter() is False
